c044067504f7850814e272f5e0f3e0e920be7d71
qing.liu <qing.liu@socialcredits.cn>
2017-12-27 18:03:53 +0800

    修改为懒加载

--- src/index.js
+++ src/index.js
@@ -1,41 +1,20 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 import './style.css';
-// import Icon from './icon.png';
-// import printMe from './print.js';
-import { cube } from './math.js';
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
-}
 function component() {
-  // var element = document.createElement('div');
-  // var btn = document.createElement('button');
-  // // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.classList.add('hello');
-  // var myIcon = new Image();
-  // myIcon.src = Icon;
-  // element.appendChild(myIcon);
+  var element = document.createElement('div');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+  button.innerHTML = 'Click me and look at the console!';
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;
+    print();
+  })
 
-  // btn.innerHTML = 'Click me and check the console!';
-  // btn.onclick = printMe;
-  // element.appendChild(btn);
-
-  var preElement = document.createElement('pre');
-    preElement.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
-  return preElement;
+  return element;
 }
-let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
 document.body.appendChild(component());
-
-// if (module.hot) {
-//   module.hot.accept('./print.js', function () {
-//     console.log('Accepting the updated printMe module!');
-//     // printMe();
-//     document.body.removeChild(element);
-//     element = component(); // 重新渲染页面后，component 更新 click 事件处理
-//     document.body.appendChild(component());
-//   })
-// }
